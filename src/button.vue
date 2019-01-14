9ab491044651ef9034bb39697b9e777df0df45b7
Wallen <JackWong992@163.com>
2019-01-14 17:40:18 +0800

    button转为单文件组件、igitinore取消上传.cache dist目录

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button class="g-button">你好</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // name: "button"
+    }
+</script>
+
+<style scoped lang="scss">
+    .g-button {
+        padding: 0 1em;
+        height: var(--button-height);
+        font-size: var(--font-size);
+        background: var(--button-bg);
+        border-radius: var(--border-radius);
+        color: var(--color);
+        border: 1px solid  var(--border-color);
+        &:hover {
+            background: var(--border-color-hover);
+        }
+        &:active {
+            background: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
