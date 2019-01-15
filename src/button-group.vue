3535f2d2eef6f87fc5813df44b38fa4516aa4a9d
Wallen <JackWong992@163.com>
2019-01-15 11:42:35 +0800

    添加button-group组件

---
+++ src/button-group.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group"
+    }
+</script>
+
+<style scoped lang="scss">
+    .button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            border-radius: 0;
+            border-left: none;
+            &:first-child {
+                border-left: 1px solid var(--border-color);
+                border-bottom-left-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-bottom-right-radius: var(--border-radius);
+                border-top-right-radius: var(--border-radius);
+            }
+        }
+    }
+</style>
