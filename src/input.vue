29ed2d40d1e80e08c954f9cf500fd6397ef5452b
Wallen <JackWong992@163.com>
2019-01-16 17:23:16 +0800

    input 添加error 和 info两种状态

--- src/input.vue
+++ src/input.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper" :class="{error}">
         <input
                 v-bind:value="value"
                 :disabled="disabled"
                 :readonly="readonly"
                 type="text">
+        <template>
+            <icon name="error" v-if="error" class="icon-error"></icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+        <template>
+            <icon name="info" v-if="info" class="icon-info"></icon>
+            <span class="info-message">{{info}}</span>
+        </template>
     </div>
 </template>
 
 <script>
+    import Icon from './icon'
     export default {
         name: 'g-input',
         props: {
@@ -22,7 +31,16 @@
             readonly: {
               type: Boolean,
               default: false
+            },
+            error: {
+              type: String
+            },
+            info: {
+              type: String
             }
+        },
+        components: {
+            Icon
         }
     }
 </script>
@@ -36,31 +54,30 @@
     $box-shadow-color: rgba(0,0,0,.5);
     $input-padding: 0 8px;
     $input-disabled-color: #bbb;
+    $error-color: #F1435D;
     .wrapper {
         font-size: $font-size;
-        display: inline-block;
-        > input {
-            height: $height;
-            border: 1px solid $border-color;
-            border-radius: $border-radius;
-            padding: $input-padding;
-            font-size: inherit;
-            &:hover {
-                border-color: $border-color-hover;
-            }
-            &:focus {
-                outline: none;
-                box-shadow: inset 0 1px 3px $box-shadow-color;
-            }
-            &[disabled]{
-                color: $input-disabled-color;
-                cursor: not-allowed;
-                border-color: $input-disabled-color;
-            }
-            &[readonly]{
-                box-shadow: none;
-                /*cursor: not-allowed;*/
+        display: inline-flex;
+        align-items: center;
+        > * {
+            margin-right: .3em;
+            &:last-child{
+                margin-right: 0;
             }
         }
+        > input {height: $height;border: 1px solid $border-color;border-radius: $border-radius;padding: $input-padding;font-size: inherit;
+            &:hover {border-color: $border-color-hover;}
+            &:focus {outline: none;box-shadow: inset 0 1px 3px $box-shadow-color;}
+            &[disabled]{color: $input-disabled-color;cursor: not-allowed;border-color: $input-disabled-color;}
+            &[readonly]{box-shadow: none; /*cursor: not-allowed;*/}
+        }
+        &.error {
+            > input {border: 1px solid $error-color;}
+        }
+        
+        .icon-error {fill: $error-color;}
+        .error-message {color: $error-color;}
+        .icon-info {fill: #46a0fc;}
+
     }
 </style>
