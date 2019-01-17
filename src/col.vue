d41f7ef186aa40a19836e686aad305b572b74e47
Wallen <JackWong992@163.com>
2019-01-17 21:55:26 +0800

    增加span && offset属性

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,41 @@
 <template>
-
+  <div class="col" :class="[`col-${span}`, offset&&`offset-${offset}` ]">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "col"
+    // name: "ifeelCol"
+    props: {
+      span: {
+        type: [String,Number]
+      },
+      offset: {
+        type: [String,Number]
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .col {
+    display: flex;
+    width: 50%;
+    height: 100px;
+    border: 1px solid red;
+    background-color: gray;
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{
+        margin-left: ($n/24)*100%;
+      }
+    }
+  }
 </style>
